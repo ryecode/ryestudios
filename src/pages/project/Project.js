19c6b95a55f7f021f743c18e6c77d7d36d55e99c
Rye Corral <132730248+ryecode@users.noreply.github.com>
2023-08-15 18:57:15 +0800

    attempt to fix vercel deployment bug

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -8,19 +8,28 @@ export const Project = () => {
   return (
     <motion.div
       className="projectBody"
-    //   initial={{ height: "100%" }}
-    //   animate={{ height: "100%" }}
-    //   exit={{ y: "-100%", transition: {duration: 0.6, delay: 0.1 } }}
+      //   initial={{ height: "100%" }}
+      //   animate={{ height: "100%" }}
+      //   exit={{ y: "-100%", transition: {duration: 0.6, delay: 0.1 } }}
 
-    // initial={{opacity: 0, transition: {duration: 1, delay: 0.2}}}
-    // animate={{opacity: 1, transition: {duration: 1, delay: 0.2}}}
-    // exit={{opacity: 0, transition: {duration: 1, delay: 0.2}}}
+      // initial={{opacity: 0, transition: {duration: 1, delay: 0.2}}}
+      // animate={{opacity: 1, transition: {duration: 1, delay: 0.2}}}
+      // exit={{opacity: 0, transition: {duration: 1, delay: 0.2}}}
 
-    initial={{scale: 0, transition: {duration: 2}}}
-    animate={{scale: 1, transition: {duration: 1, delay: 0.2}}}
-    exit={{scale: 0, transition: {duration: 1, delay: 0.2}}}
+      initial={{ scale: 0, transition: { duration: 2 } }}
+      animate={{ scale: 1, transition: { duration: 1, delay: 0.2 } }}
+      exit={{ scale: 0, transition: { duration: 1, delay: 0.2 } }}
     >
-      <Card3D />
+      <div className="header">
+        <h1 class="hero-title">
+          <b>GUEST, Check out my projects!</b>
+        </h1>
+      </div>
+      <div className="profileCard">
+        <div>
+          <Card3D />
+        </div>
+      </div>
     </motion.div>
   );
 };
