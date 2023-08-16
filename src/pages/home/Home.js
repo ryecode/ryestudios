148fa932effe29d6e4843eef377a33419b586ba3
Rye Corral <132730248+ryecode@users.noreply.github.com>
2023-08-16 10:31:16 +0800

    Updated file type of poster after Vercel TS

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -29,7 +29,7 @@ return (
       muted
       playsInline
       fetchPriority={"high"}
-      poster="https://ryecode.github.io/portfolio/ImageBank/RYELogoV2.jpeg"
+      poster="https://ryecode.github.io/portfolio/ImageBank/RYELogoV2.png"
     />
     <div className="content">
       <h1>WELCOME</h1>
