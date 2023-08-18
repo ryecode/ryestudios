9dd368da3dfe020b0738c74e5b898e273da3456b
Rye Corral <132730248+ryecode@users.noreply.github.com>
2023-08-18 13:31:16 +0800

    Update for useContext

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import styles from './global.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Navbar } from './components/navbar/Navbar';
 import { Footer } from './components/footer/Footer';
-import React from "react";
+import  { React, useState, createContext } from "react";
 import { AnimatedRoutes } from './components/AnimatedRoutes';
 
-
+export const AppContext = createContext();
 
 const App = () => {
-
+  const [userName, setUserName] = useState("");
 
 
   return <div className={styles.App}>
+    <AppContext.Provider value={{ userName, setUserName }}>
     <Router>
       <Navbar />
         <AnimatedRoutes />
       <Footer />
     </Router>
+    </AppContext.Provider>
   </div>
 };
 export default App;
