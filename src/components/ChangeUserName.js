7f51ada835b4a2727828b6a465fb2ee580d3fa1b
Rye Corral <132730248+ryecode@users.noreply.github.com>
2023-08-19 16:58:35 +0800

    Updated modal formats, return arrow pointer.

--- src/components/ChangeUserName.js
+++ src/components/ChangeUserName.js
@@ -9,7 +9,7 @@ export const ChangeUserName = () => {
   const { userName } = useContext(AppContext);
   return (
     <>
-      <div id="askName">
+      <div id="askName" className="modalTrigger">
         <h4>
           Greetings Guest,
           <br />
@@ -19,6 +19,7 @@ export const ChangeUserName = () => {
           onChange={(event) => {
             setNewUserName(event.target.value);
           }}
+          style={{padding:"0.2em", textAlign:"center"}}
         />
         <br />
         <button
@@ -30,6 +31,7 @@ export const ChangeUserName = () => {
         >
           <a href="#demo-modal">ENTER</a>
         </button>
+
         <div id="demo-modal" className="modal wrapper">
           <div class="modal__content">
             <div class="modal__close">
@@ -41,29 +43,39 @@ export const ChangeUserName = () => {
               Hi{""} {userName}!
             </h1>
             <p className="modal__footer">
-              Thank you for visiting my website. I do hope you will like what
-              you see here. So please go ahead, explore, and enjoy!
+              Thank you for visiting my website. I do hope you will like
+              portfolio. Please go ahead, explore, and enjoy!
               <br />
-              <img
-                src="https://ryecode.github.io/portfolio/ImageBank/rocket.png"
-                alt="rocket ship"
-                width={"25%"}
-                style={{ zIndex: 1 }}
-              />
             </p>
 
             {/* <a href="/profile" class="modal__close1">
             <p style={{color:"black"}}>CHECK RYE PROFILE</p>
           </a> */}
 
-            {/* <a href="#pointer" class="modal__close2">            
-            <p style={{color:"black"}}>START EXPLORING!</p>
-          </a> */}
+            <a href="#pointer" class="modal__close2">
+              <img
+                src="https://ryecode.github.io/portfolio/ImageBank/rocket.png"
+                alt="rocket ship"
+                width={"70%"}
+                style={{ zIndex: 1 }}
+              />
+              <p style={{ color: "black", fontSize: "0.6em" }}>
+                START EXPLORING!
+              </p>
+            </a>
 
             {/* <a href="/contact" class="modal__close3">
           <p style={{color:"black"}}>CONTACT RYE</p>        
           </a> */}
           </div>
+        </div>
+
+        <div id="pointer" className="modalPointer">
+          <img
+            src="https://ryecode.github.io/portfolio/ImageBank/upArrow.png"
+            alt="pointer"
+            width={"20%"}
+          />
         </div>
       </div>
     </>
